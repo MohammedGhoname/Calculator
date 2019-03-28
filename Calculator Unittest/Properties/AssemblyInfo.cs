677f0ec75e16e2634d9a2dbd1f5e3492ea7d51af
Mohammed Ghoname <bslsmm@hotmail.com>
2019-03-28 12:26:36 +0300

    First Commit

---
+++ Calculator Unittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator Unittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator Unittest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1910719-8c00-4ae2-b34c-b57b522f0dee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Calculator Unittest/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalcTest;
+namespace Calculator_Unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Calculator_Sum_ValidValue()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.sum(2, 5);
+            Assert.AreEqual(7, result);
+        }
+
+
+        [TestMethod]
+        public void Calculator_Sum_ValidValue2()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.sum(3, 7);
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public void Calculator_Sum_InValidValue()
+        {
+            Calculator calculator = new Calculator();
+            var result = calculator.sum(2, 5);
+            Assert.AreNotEqual(6, result);
+        }
+    }
+}
